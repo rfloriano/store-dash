fb10dc12a10128fb63f63c8ca409ef2acd2fe3da
Rafael Floriano da Silva <rflorianobr@gmail.com>
2015-02-23 22:44:20 -0300

    Added proxy for staging

--- pydashie/samplers/omni_versions.py
+++ pydashie/samplers/omni_versions.py
@@ -20,14 +20,30 @@ class OmniVersions(DashieSampler):
             if 'local' in app.get('url')\
                or 'local' in app.get('title', '').lower():
                 continue
+
+            proxies = {}
+            if 'staging' in app.get('url')\
+               or 'staging' in app.get('title', '').lower()\
+               or 'stg' in app.get('url')\
+               or 'stg' in app.get('title', '').lower():
+                proxies = {
+                    'http': 'http://proxy.staging.globoi.com:3128',
+                    'http': 'http://proxy.staging.globoi.com:3128',
+                }
             url = os.path.join(app.get('url'), 'admin/omni-store/version/')
             print 'Getting version from {0}({1})'.format(app.get('title'), url)
+            resp = None
+            data = {}
             try:
-                data = requests.get(url, timeout=3).json()
-            except Exception:
-                data = {}
+                resp = requests.get(url, timeout=3, proxies=proxies)
+                data = resp.json()
+            except (requests.exceptions.ConnectionError,
+                    requests.exceptions.ReadTimeout, ValueError):
+                pass
+            if hasattr(resp, 'status_code') and resp.status_code == 404:
+                data['version'] = 'none'
             items.append({
-                'label': app.get('title', 'undef'),
-                'value': data.get('version', 'undef')
+                'label': app.get('title', 'error'),
+                'value': data.get('version', 'error')
             })
         return {'items': items}
