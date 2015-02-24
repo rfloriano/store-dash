1a0bda4af44870eff2c0868423a779d6ec8d1759
Rafael Floriano da Silva <rflorianobr@gmail.com>
2015-02-24 13:15:15 -0300

    store api has logic to get omnistore versions

--- pydashie/samplers/omni_versions.py
+++ pydashie/samplers/omni_versions.py
@@ -1,6 +1,5 @@
 from pydashie.dashie_sampler import DashieSampler
 
-import os
 import requests
 
 
@@ -17,33 +16,8 @@ class OmniVersions(DashieSampler):
         applications = resp.json().get('applications', [])
         items = []
         for app in applications:
-            if 'local' in app.get('url')\
-               or 'local' in app.get('title', '').lower():
-                continue
-
-            proxies = {}
-            if 'staging' in app.get('url')\
-               or 'staging' in app.get('title', '').lower()\
-               or 'stg' in app.get('url')\
-               or 'stg' in app.get('title', '').lower():
-                proxies = {
-                    'http': 'http://proxy.staging.globoi.com:3128',
-                    'https': 'http://proxy.staging.globoi.com:3128',
-                }
-            url = os.path.join(app.get('url'), 'admin/omni-store/version/')
-            print 'Getting version from {0}({1})'.format(app.get('title'), url)
-            resp = None
-            data = {}
-            try:
-                resp = requests.get(url, timeout=3, proxies=proxies)
-                data = resp.json()
-            except (requests.exceptions.ConnectionError,
-                    requests.exceptions.ReadTimeout, ValueError):
-                pass
-            if hasattr(resp, 'status_code') and resp.status_code == 404:
-                data['version'] = 'none'
             items.append({
                 'label': app.get('title', 'error'),
-                'value': data.get('version', 'error')
+                'value': app.get('omni-store', {}).get('version', 'error')
             })
         return {'items': items}
