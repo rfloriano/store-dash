9e901dcac18c3ba77da69702032d009f0d387ef4
Marcelo Jorge Vieira <metal@alucinados.com>
2015-07-02 18:51:40 -0300

    Added installer and omnistore versions

--- pydashie/samplers/backstage_store_versions.py
+++ pydashie/samplers/backstage_store_versions.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import requests
+from distutils.version import LooseVersion
 
 from pydashie.dashie_sampler import DashieSampler
 
@@ -23,6 +24,27 @@ class BackstageStoreVersions(DashieSampler):
             try:
                 resp = requests.get(url)
                 version = resp.json().get('version', '')
+            except:
+                version = ''
+            items.append({'label': label, 'value': version})
+
+        data = [
+            ('OmniStore', True, 'http://artifactory.globoi.com/artifactory/api/storage/pypi-local/omnistore'),
+            ('OmniInstaller', False, 'http://artifactory.globoi.com/artifactory/api/storage/pypi-local/omni-installer'),
+        ]
+
+        for label, aux, url in data:
+            try:
+                resp = requests.get(url)
+                versions = resp.json().get('children')
+                versions = [x['uri'].replace('/', '') for x in versions]
+                aux_version = ''
+                if aux:
+                    branch_version = [x for x in versions if x.startswith('0.4')]
+                    branch_version.sort(key=LooseVersion)
+                    items.append({'label': 'OmniStore 0.4', 'value': branch_version[-1]})
+                versions.sort(key=LooseVersion)
+                version = "%s%s" % (versions[-1], aux_version)
             except:
                 version = ''
             items.append({'label': label, 'value': version})
